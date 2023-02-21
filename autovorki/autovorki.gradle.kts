5a0010c2dfaa366e89b72613661ac699a1da9acb
BufferOverNo <volatiledesign@outlook.com>
2023-02-21 10:04:23 +0000

    Update autovorki.gradle.kts

--- autovorki/autovorki.gradle.kts
+++ autovorki/autovorki.gradle.kts
@@ -50,7 +50,7 @@ tasks {
             attributes(mapOf(
                     "Plugin-Version" to project.version,
                     "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
-                    "Plugin-Provider" to "Tea",
+                    "Plugin-Provider" to "Swifty",
                     "Plugin-Dependencies" to
                         arrayOf(
                             nameToId("iUtils")
